cbb0493c01cdedf1997293b68f2b04ff9fe7f658
Bazinga09 <98630904+Bazinga09@users.noreply.github.com>
2022-08-01 20:24:03 +0530

    fr_ready

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+
+function Header() {
+  return (
+    <div className="app-root-1">
+      <header className="Paper-root AppBar-root AppBar-positionStatic AppBar-colorPrimary Paper-elevation4">
+        <div className="Toolbar-root Toolbar-regular header-appBar-13 Toolbar-gutters">
+          <div className="header-left-4"></div>
+          <img className="header-logo-11" src={logo} />
+          <div className="header-vertical-9"></div>
+          <div className="header-empty-6"></div>
+          <button className="button-56" role="button">
+            MINT YOUR NFT IN JUST ONE CLICK !!
+          </button>
+          <div className="header-empty-6"></div>
+          <div className="header-space-8"></div>
+          <button className="button-85" role="button">CONNECT PLUG  WALLET</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
